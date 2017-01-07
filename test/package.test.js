673e397ee07516ebd9ecba9fef82d03b5fc76e31
Jared Hanson <jaredhanson@gmail.com>
2017-01-08 08:57:54 +1300

    Add tests.

--- test/package.test.js
+++ test/package.test.js
@@ -8,7 +8,17 @@ describe('@authnomicon/sso-oauth2', function() {
   
   it('should export manifest', function() {
     expect(pkg).to.be.an('object');
+    expect(pkg['createprovider']).to.be.a('function');
     expect(pkg['statestore']).to.be.a('function');
+  });
+  
+  describe('createprovider', function() {
+    var createprovider = pkg['createprovider'];
+    
+    it('should be annotated', function() {
+      expect(createprovider['@implements']).to.equal('http://schemas.authnomicon.org/js/sso/oauth2/createProvider');
+      expect(createprovider['@singleton']).to.equal(true);
+    });
   });
   
   describe('statestore', function() {
