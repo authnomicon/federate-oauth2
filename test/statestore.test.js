02f3fb582222b386e47cc8ffc3dc41e9bfa86e28
Jared Hanson <jaredhanson@gmail.com>
2017-01-08 09:01:17 +1300

    Add tests.

---
+++ test/statestore.test.js
@@ -0,0 +1,19 @@
+/* global describe, it, expect */
+
+var expect = require('chai').expect;
+var sinon = require('sinon');
+var factory = require('../lib/statestore');
+
+
+describe('statestore', function() {
+  
+  it('should export factory function', function() {
+    expect(factory).to.be.a('function');
+  });
+  
+  it('should be annotated', function() {
+    expect(factory['@implements']).to.equal('http://schemas.authnomicon.org/js/sso/oauth2/StateStore');
+    expect(factory['@singleton']).to.equal(true);
+  });
+  
+});
