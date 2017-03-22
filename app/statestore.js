4d84dc1851ccfb500780f2ea541e5bf40619053c
Jared Hanson <jaredhanson@gmail.com>
2017-03-21 20:43:59 -0700

    Reorg.

--- app/statestore.js
+++ app/statestore.js
@@ -1,55 +1,8 @@
-function CeremonyStateStore(store) {
-  this._store = store;
-}
-
-CeremonyStateStore.prototype.store = function(req, meta, cb) {
-  // https://tools.ietf.org/html/draft-bradley-oauth-jwt-encoded-state-06
+exports = module.exports = function(store) {
+  var CommonStateStore = require('../lib/commonstatestore');
   
-  var state = {
-    name: 'sso/oauth2',
-    authorizationURL: meta.authorizationURL,
-    clientID: meta.clientID
-  };
-  var ps = (req.query && req.query.state) || (req.body && req.body.state);
-  if (ps) { state.prev = ps; }
   
-  this._store.save(req, state, function(err, h) {
-    if (err) { return cb(err); }
-    return cb(null, h);
-  });
-}
-
-CeremonyStateStore.prototype.verify = function(req, h, meta, cb) {
-  function loaded(err, state) {
-    if (err) { return cb(err); }
-    if (!state) { return cb(null, false); }
-    
-    // TODO: Destroy the state??
-    
-    if (state.name !== 'sso/oauth2') {
-      return cb(null, false);
-    }
-    if (state.authorizationURL !== meta.authorizationURL) {
-      return cb(null, false);
-    }
-    if (state.clientID !== meta.clientID) {
-      return cb(null, false);
-    }
-    return cb(null, true, state);
-  }
-  
-  if (req.state) {
-    loaded(null, req.state);
-  } else {
-    this._store.load(req, h, loaded);
-  }
-}
-
-
-
-
-exports = module.exports = function(s) {
-  var store = new CeremonyStateStore(s);
+  var store = new CommonStateStore(store);
   return store;
 };
 
