a63b365c66a0899000c80707a6f039d178bcbf6e
Jared Hanson <jaredhanson@gmail.com>
2017-05-18 14:49:33 -0700

    Add COTC.

---
+++ app/http/cotc.js
@@ -0,0 +1,15 @@
+exports = module.exports = function(challengeHandler, responseHandler) {
+  var express = require('express');
+  var router = new express.Router();
+  
+  router.get('/', challengeHandler);
+  router.post('/', responseHandler);
+  
+  return router;
+};
+
+exports['@implements'] = 'http://schemas.authnomicon.org/js/federate/oauth2/http/COTCService';
+exports['@require'] = [
+  './handlers/cotc/challenge',
+  './handlers/cotc/response'
+];
