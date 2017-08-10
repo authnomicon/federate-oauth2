1ba6cf9c57bfee75de1f52bcbccfc0486ea4358d
Jared Hanson <jaredhanson@gmail.com>
2017-08-09 20:45:54 -0700

    Remove createprovider.

--- app/createprovider.js
+++
@@ -1,7 +0,0 @@
-exports = module.exports = function(container) {
-  return container.create('./createprovider/used');
-};
-
-exports['@implements'] = 'http://schemas.authnomicon.org/js/sso/oauth2/createProvider';
-exports['@singleton'] = true;
-exports['@require'] = [ '!container' ];
